5cfa6d078b43c3e2dc9d4ef41ec64f1e722857fb
MrEnder0 <72358826+MrEnder0@users.noreply.github.com>
2024-01-13 19:04:51 -0600

    Big ui update with preview and lighting bug fix

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,10 @@
+use eframe::egui;
 use image::{imageops::FilterType, GenericImageView};
 use screenshots::Screen;
 use std::ffi::CStr;
 use wooting_rgb_sys as wooting;
-use eframe::egui;
-use tray_icon::TrayIconBuilder;
-
-#[cfg(not(target_os = "linux"))]
-use std::{cell::RefCell, rc::Rc};
 
 fn main() -> Result<(), eframe::Error> {
-    let path = concat!(env!("CARGO_MANIFEST_DIR"), "/media/icon.png");
-    let icon = load_icon(std::path::Path::new(path));
-
-    #[cfg(target_os = "linux")]
-    std::thread::spawn(|| {
-        use tray_icon::menu::Menu;
-
-        gtk::init().unwrap();
-        let _tray_icon = TrayIconBuilder::new()
-            .with_menu(Box::new(Menu::new()))
-            .with_icon(icon)
-            .build()
-            .unwrap();
-
-        gtk::main();
-    });
-
-    #[cfg(not(target_os = "linux"))]
-    let mut _tray_icon = Rc::new(RefCell::new(None));
-    #[cfg(not(target_os = "linux"))]
-    let tray_c = _tray_icon.clone();
-
     // Run to reset rgb
     unsafe {
         wooting::wooting_rgb_array_update_keyboard();
@@ -39,20 +13,14 @@ fn main() -> Result<(), eframe::Error> {
     eframe::run_native(
         "Wootili-View",
         eframe::NativeOptions::default(),
-        Box::new(move |_cc| {
-            #[cfg(not(target_os = "linux"))]
-            {
-                tray_c
-                    .borrow_mut()
-                    .replace(TrayIconBuilder::new().with_icon(icon).build().unwrap());
-            }
-            Box::<MyApp>::default()
-        }),
+        Box::new(move |_cc| Box::<MyApp>::default()),
     )
 }
 
 struct MyApp {
     rgb_size: (u32, u32),
+    brightness: u8,
+    screen: usize,
 }
 
 impl Default for MyApp {
@@ -72,47 +40,77 @@ impl Default for MyApp {
                     | "Wooting Two LE"
                     | "Wooting Two HE"
                     | "Wooting Two HE (ARM)" => (21, 6),
-                    "Wooting 60HE" | "Wooting 60HE (ARM)" => (14, 6),
+                    "Wooting 60HE" | "Wooting 60HE (ARM)" => (14, 5),
                     "Wooting UwU" | "Wooting UwU RGB" => (3, 1),
                     _ => {
                         println!("Unsupported keyboard model: {}", model.to_str().unwrap());
-                        return Self { rgb_size: (0, 0) };
+                        (0, 0)
                     }
                 }
             },
+            brightness: 100,
+            screen: 0,
         }
     }
 }
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        use tray_icon::TrayIconEvent;
-
-        if let Ok(event) = TrayIconEvent::receiver().try_recv() {
-            println!("tray event: {event:?}");
-        }
-
         let screens = Screen::all().unwrap();
-        let capture = screens[0].capture().unwrap();
-        
-        let img = image::ImageBuffer::from_raw(capture.width(), capture.height(), capture.to_vec()).unwrap();
-        let img = image::DynamicImage::ImageRgba8(img);
-        let resized_capture = img.resize_exact(self.rgb_size.0, self.rgb_size.1, FilterType::Nearest);
+        let capture = screens[self.screen].capture().unwrap();
 
-        resized_capture.save("preview.png").unwrap();
+        let img = image::ImageBuffer::from_raw(capture.width(), capture.height(), capture.to_vec())
+            .unwrap();
+        let img = image::DynamicImage::ImageRgba8(img);
+        let resized_capture =
+            img.resize_exact(self.rgb_size.0, self.rgb_size.1, FilterType::Nearest);
 
         // Runs lighting operations
         unsafe {
             for (x, y, pixel) in resized_capture.pixels() {
                 let image::Rgba([r, g, b, _]) = pixel;
-                wooting::wooting_rgb_array_set_single(y as u8, x as u8, r, g, b);
+                wooting::wooting_rgb_array_set_single(
+                    y as u8 + 1,
+                    x as u8,
+                    (r as f32 * (self.brightness as f32 * 0.01)).round() as u8,
+                    (g as f32 * (self.brightness as f32 * 0.01)).round() as u8,
+                    (b as f32 * (self.brightness as f32 * 0.01)).round() as u8,
+                );
             }
 
             wooting::wooting_rgb_array_update_keyboard();
         }
 
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("TODO: Add settings here");
+            ui.heading("Settings");
+
+            ui.add(egui::Slider::new(&mut self.brightness, 50..=150).text("Brightness"));
+            ui.add(egui::Slider::new(&mut self.screen, 0..=screens.len() - 1).text("Screen"));
+
+            egui::SidePanel::right("lighting_preview_panel").show(ctx, |ui| {
+                ui.heading("Preview Lighting");
+                for y in 0..self.rgb_size.1 {
+                    //add a row
+                    ui.horizontal(|ui| {
+                        for x in 0..self.rgb_size.0 {
+                            let color: egui::Color32 = {
+                                let image::Rgba([r, g, b, _]) = resized_capture.get_pixel(x, y);
+
+                                egui::Color32::from_rgb(r, g, b)
+                            };
+
+                            let size = egui::Vec2::new(10.0, 10.0);
+                            let rect = ui.allocate_space(size);
+                            ui.painter().rect_filled(rect.1, 1.0, color);
+                        }
+                    });
+                }
+
+                ui.add(egui::Label::new(format!(
+                    "RGB Size: {} rows and {} collums",
+                    self.rgb_size.0, self.rgb_size.1
+                )));
+            });
         });
 
         std::thread::sleep(std::time::Duration::from_millis(10));
@@ -125,16 +123,4 @@ impl eframe::App for MyApp {
             wooting::wooting_rgb_close();
         }
     }
-}
-
-fn load_icon(path: &std::path::Path) -> tray_icon::Icon {
-    let (icon_rgba, icon_width, icon_height) = {
-        let image = image::open(path)
-            .expect("Failed to open icon path")
-            .into_rgba8();
-        let (width, height) = image.dimensions();
-        let rgba = image.into_raw();
-        (rgba, width, height)
-    };
-    tray_icon::Icon::from_rgba(icon_rgba, icon_width, icon_height).expect("Failed to open icon")
 }
